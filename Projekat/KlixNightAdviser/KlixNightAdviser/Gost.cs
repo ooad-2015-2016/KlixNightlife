e182c71594e810d564e770659123e8a47ae190d2
ekalac1 <elzaa_95@hotmail.com>
2016-04-22 14:29:54 +0200

    Klase gost i korisnik dodane

---
+++ Projekat/KlixNightAdviser/KlixNightAdviser/Gost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlixNightAdviser
+{
+    class Gost
+    {
+        public Gost(string id)
+        { this.id = id; }
+        string id { get; set; }
+        
+    }
+}
---
+++ Projekat/KlixNightAdviser/KlixNightAdviser/Korisnik.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlixNightAdviser
+{
+    public enum Spol { muski, zenski};
+    class Korisnik : Gost
+    {
+        public Korisnik(string imePrezime, string korisnickoIme, string lozinka, DateTime datumRodjenja, string mail, Spol s) : base(korisnickoIme)
+        {
+            Naziv = imePrezime;
+            Lozinka = lozinka;
+            DatumRođenja = datumRodjenja;
+            eMail = mail;
+            DatumRegistracije = DateTime.Now;
+            spol = s;
+            
+        }
+        string Naziv { get; set; }
+        DateTime DatumRođenja { get; }
+        string Lozinka;
+        string eMail { get; set; }
+        DateTime DatumRegistracije { get; }
+        Spol spol;
+
+
+
+    }
+}
