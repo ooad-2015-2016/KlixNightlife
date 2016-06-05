51780826f1f866690afc1fa2a82115a2a56232e7
Amer Kadic <akadic5@etf.unsa.ba>
2016-06-05 21:19:35 +0200

    Igra

---
+++ Igra/Assets/Game/Script/Game.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+
+    class Game : MonoBehaviour
+{
+    private float cooldown = 30;
+    public float cooldownAmount = 30;
+    GameObject gameLogic;
+
+    void Start()
+    {
+        gameLogic = GameObject.Find("GameLogic");
+
+    }
+
+    void Update()
+        {
+        /*  Ne radi iz nekog razloga.
+            GameObject[] Targets = GameObject.FindGameObjectsWithTag("Target");
+            int t = Targets.Length;
+            if ( t == 4)
+     */
+            if (cooldown < 0)
+            {
+            gameLogic.GetComponent<GameLogic>().displayMenu();
+            }
+            cooldown -= Time.deltaTime;
+
+        }
+    
+}
